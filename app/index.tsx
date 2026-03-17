189788896100891e4be589171f2294a2a7274767
Gustavo Maia <gustavopmaia@yahoo.com.br>
2026-03-17 16:36:17 -0300

    fix: make ID a number instead of string

--- app/index.tsx
+++ app/index.tsx
@@ -1,17 +1,17 @@
 import { View, Text, TouchableOpacity, StyleSheet, ScrollView } from "react-native";
 
 type Book = {
-  id: string;
+  id: number;
   title: string;
   author: string;
   year: number;
 };
 
 const books: Book[] = [
-  { id: "1", title: "Teste 1", author: "Gabriel Fidalgo", year: 1234 },
-  { id: "2", title: "Teste 2", author: "Gustavo Maia", year: 1234 },
-  { id: "3", title: "Teste 3", author: "Gustavo Rossi", year: 1234 },
-  { id: "4", title: "Teste 4", author: "Pedro Lima", year: 1234 },
+  { id: 1, title: "Teste 1", author: "Gabriel Fidalgo", year: 1234 },
+  { id: 2, title: "Teste 2", author: "Gustavo Maia", year: 1234 },
+  { id: 3, title: "Teste 3", author: "Gustavo Rossi", year: 1234 },
+  { id: 4, title: "Teste 4", author: "Pedro Lima", year: 1234 },
 ];
 
 export default function Home() {
