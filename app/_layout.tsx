dd68db642733bd99b48a9f1d51a519eb159e1f77
Gustavo Maia <67283753+gustavopmaia@users.noreply.github.com>
2026-03-17 19:27:37 -0300

    fix: Create an object with the tabs style properties

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,37 +1,40 @@
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
+import { StyleSheet } from "react-native";
 
 export default function Layout() {
   return (
     <Tabs
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "#0F1115",
-        },
-        headerTitleStyle: {
-          color: "#FFFFFF",
-          fontWeight: "700",
-        },
-        headerTintColor: "#E83D84",
-        tabBarStyle: {
-          backgroundColor: "#12161D",
-          borderTopColor: "#262B36",
-          height: 62,
-          paddingBottom: 8,
-          paddingTop: 8,
-        },
-        tabBarActiveTintColor: "#E83D84",
-        tabBarInactiveTintColor: "#8E8E93",
-      }}
+      screenOptions={screenStyle}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "Biblioteca Virtual",
           tabBarLabel: "Início",
-          tabBarIcon: ({ color, size }) => <Ionicons name="home" size={size} color={color} />,
+          tabBarIcon: ({ color, size }: { color: number, size: number }) => <Ionicons name="home" size={size} color={color} />,
         }}
       />
     </Tabs>
   );
+}
+
+const screenStyle = {
+  headerStyle: {
+    backgroundColor: "#0F1115",
+  },
+  headerTitleStyle: {
+    color: "#FFFFFF",
+    fontWeight: "700",
+  },
+  headerTintColor: "#E83D84",
+  tabBarStyle: {
+    backgroundColor: "#12161D",
+    borderTopColor: "#262B36",
+    height: 62,
+    paddingBottom: 8,
+    paddingTop: 8,
+  },
+  tabBarActiveTintColor: "#E83D84",
+  tabBarInactiveTintColor: "#8E8E93",
 }
